a4aeed60eeb503855e537ae83ee5f09298f72815
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-03-13 14:02:06 +0500

    UserService added

---
+++ Udemy.DataAccess/Repositories/Genericss.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.DataAccess.Context;
+
+namespace Udemy.DataAccess.Repositories
+{
+    public class Genericss<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Genericss(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async ValueTask<TEntity> InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async ValueTask<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async ValueTask<TEntity> DeleteAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async ValueTask<TEntity> SelectAsync(long id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public IEnumerable<TEntity> SelectAllAsEnumerable()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public IQueryable<TEntity> SelectAllAsQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async ValueTask SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Udemy.Services/Services/UserServices.cs
+++ Udemy.Services/Services/UserServices.cs
@@ -1,37 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Udemy.Domain.Entites;
 using Udemy.Services.DTOs.Users;
 using Udemy.Services.Interfaces;
+using Udemy.DataAccess.Repositories;
+using Udemy.Services.Helpers;
+
 namespace Udemy.Services.Services;
 
 public class UserService : IUserService
 {
-    public GenericRepository context { get; set; }
+    private readonly Genericss<User> context;
     public async ValueTask<UserView> CreateAsync(UserCreate userCreation)
     {
-        throw new NotImplementedException();
+        await context.InsertAsync(userCreation.MapTo<User>());
+        await context.SaveAsync();
+        return userCreation.MapTo<UserView>();
     }
 
     public async ValueTask<bool> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await context.SelectAsync(id);
+        await context.DeleteAsync(user);
+        await context.SaveAsync();
+        return true;
     }
 
     public async ValueTask<UserView> GetAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await context.SelectAsync(id);
+        return user.MapTo<UserView>();
     }
 
     public async ValueTask<List<UserView>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var users=context.SelectAllAsEnumerable();
+        return  users.ToList().Select(x => x.MapTo<UserView>()).ToList();
     }
 
     public async ValueTask<UserView> UpdateAsync(int id, UserCreate userViewModel)
     {
-        throw new NotImplementedException();
+        var user = await context.SelectAsync(id);
+
+        user.UpdatedAt = DateTime.UtcNow;
+        user.FirstName= userViewModel.FirstName;
+        user.LastName= userViewModel.LastName;
+        user.PhoneNumber= userViewModel.PhoneNumber;
+        user.Password= userViewModel.Password;
+
+        await context.UpdateAsync(user);
+        await context.SaveAsync();
+        return user.MapTo<UserView>();
     }
 }
