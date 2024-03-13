860e437d885c8990ace5453c269b6b0779e9128a
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-03-13 13:21:56 +0500

    Added First Service

--- Udemy.Services/Services/UserServices.cs
+++ Udemy.Services/Services/UserServices.cs
@@ -5,34 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using Udemy.Services.DTOs.Users;
 using Udemy.Services.Interfaces;
+namespace Udemy.Services.Services;
 
-namespace Udemy.Services.Services
+public class UserService : IUserService
 {
-    public class UserService : IUserService
+    public GenericRepository context { get; set; }
+    public async ValueTask<UserView> CreateAsync(UserCreate userCreation)
     {
-        public async ValueTask<UserView> CreateAsync(UserCreate userCreation)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public async ValueTask<bool> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public async ValueTask<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async ValueTask<UserView> GetAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public async ValueTask<UserView> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async ValueTask<List<UserView>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+    public async ValueTask<List<UserView>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
 
-        public async ValueTask<UserView> UpdateAsync(int id, UserCreate userViewModel)
-        {
-            throw new NotImplementedException();
-        }
+    public async ValueTask<UserView> UpdateAsync(int id, UserCreate userViewModel)
+    {
+        throw new NotImplementedException();
     }
 }
